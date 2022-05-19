52397a92f029dc19717767e61a31ff9c2e0dc297
Andriy Kotiyga <kotygaandrey05@gmail.com>
2022-05-19 21:04:13 +0300

    - chnage images  links

--- src/main/java/com/kot/horizon/api/v1/tour/mapper/TourMapper.java
+++ src/main/java/com/kot/horizon/api/v1/tour/mapper/TourMapper.java
@@ -1,16 +1,18 @@
 package com.kot.horizon.api.v1.tour.mapper;
 
 import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import com.kot.horizon.api.v1.image.ImageController;
-import com.kot.horizon.api.v1.image.ImageUtils;
+import com.kot.horizon.api.v1.image.ImageMapper;
 import com.kot.horizon.api.v1.tour.dto.TourRequest;
 import com.kot.horizon.api.v1.tour.dto.TourResponse;
-import com.kot.horizon.image.model.ImageEntity;
 import com.kot.horizon.tour.model.TourEntity;
 
 @Component
 public class TourMapper {
+
+	@Autowired
+	private ImageMapper imageMapper;
 
 	public TourEntity toEntity(TourRequest request) {
 		TourEntity entity = new TourEntity();
@@ -26,11 +28,9 @@ public class TourMapper {
 		response.setName(entity.getName());
 		response.setDescription(entity.getDescription());
 		response.setRate(entity.getRate());
-		if (entity.getImages() != null)
-			response.setImages(entity.getImages()
-					.stream()
-					.map(ImageEntity::getImageName)
-					.collect(Collectors.toList()));
+		if (entity.getImages() != null) {
+			response.setImages(entity.getImages().stream().map(image -> imageMapper.toDto(image)).collect(Collectors.toList()));
+		}
 		return response;
 	}
 
