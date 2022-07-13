f7e86c54f06ec9ce88177e31f208e0df3ed5365c
Andriy Kotiyga <kotygaandrey05@gmail.com>
2022-07-13 20:34:46 +0300

    - fixed db

--- src/main/resources/db/migration/V1_0_10__tour_hashtags.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS hashtag
-(
-	tour_id bigint,
-	hashtag character varying(255) NOT NULL,
-	hashtag_key character varying(255) NOT NULL,
-	CONSTRAINT pk_hashtag_key PRIMARY KEY (hashtag_key),
-)
-WITH (OIDS = FALSE) TABLESPACE pg_default;
-ALTER TABLE tour_label OWNER to horizon_owner;
